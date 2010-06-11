9b53cfaa8c41e8ce340d14a94804c19c4c65f779
Matthias Tilsner <matthias@tilsner.eu>
2010-06-11 18:38:46 +0200

    Simplification of the Predicate Trait
    Actually, only one predicate trait is required. Predicates with multiple
    inputs can be constructed using tupples. Furthermore, the "test" method
    was removed since for now it is not necessary to have it.

--- src/buildingblocks/Predicate.scala
+++ src/buildingblocks/Predicate.scala
@@ -1,22 +1,7 @@
 package buildingblocks
 
-trait Predicate
-
-trait Predicate1[T] extends Predicate {
-	def call(input: T): Boolean
-	
+trait Predicate[T] {
 	def item: Symbol
-	def test(values: Map[Symbol,_]): Boolean = {
-		call(values(item).asInstanceOf[T])
-	}
-}
 
-trait Predicate2[S,T] extends Predicate {
-	def call(input: Tuple2[S,T]): Boolean
-	
-	def item1: Symbol
-	def item2: Symbol
-	def test(values: Map[Symbol,_]): Boolean = {
-		call((values(item1).asInstanceOf[S], values(item2).asInstanceOf[T]))
-	}
+	def call(input: T): Boolean
 }
