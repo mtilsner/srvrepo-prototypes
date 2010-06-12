c174bacc91ae9f6ff777b6f8ef3a4ac0f47a2cd1
Matthias Tilsner <matthias@tilsner.eu>
2010-06-12 15:57:26 +0200

    Rework of the Predicates Traits
    This is a major change on the way how predicates work. Firstmost, there
    are two different type of predicates: unary and binary predicates. These
    different types are important because they work very differently. We
    must later be able to compare the input of different predicates. If some
    take tuples this will become very hard. Also, having these two different
    predicates does not hinder us from staying with unary services.
    There are three major predicates "And", "Or", and "Not". These
    predicates can be used to combine other predicates. They are not
    implemented using some service and hence have to be treated independent.
    For predicates that are implemented using services two corresponding
    classes exist "PredicateService1" and "PredicateService2". These classes
    are wrapped in entry classes "PredicateServiceEntry1" and
    "PredicateServiceEntry2" that are bound to some specific implementation
    and scenario.
    Simple "PredicateEntry1" and "PredicateEntry2" classes exist for
    allowing the onging construction of new predicate service entries when
    encountering new scenarios (having to describe other services).
    
    At a later point, it will be necessary to match these different types of
    predicates in order to create predicatesymbols. Hence, they are created
    using case classes.

--- src/buildingblocks/Predicate.scala
+++ src/buildingblocks/Predicate.scala
@@ -1,7 +1,62 @@
 package buildingblocks
 
-trait Predicate[T] {
-	def item: Symbol
+// Predicate traits
+trait Predicate extends Symbol[Predicate] {
+	def test(values: Map[Symbol[_],_]): Boolean
+}
+trait Predicate1[T] extends Predicate {
+	def item: T
+}
+trait Predicate2[S,T] extends Predicate {
+	def item1: S
+	def item2: T
+}
 
-	def call(input: T): Boolean
+// Predicate combinators
+case class Not(_item: Predicate) extends Predicate1[Predicate] {
+	val item: Predicate = _item 
+	def test(values: Map[Symbol[_],_]): Boolean = !item.asInstanceOf[Predicate].test(values)
+}
+
+case class And(_item1: Predicate, _item2: Predicate) extends Predicate2[Predicate,Predicate] {
+	val item1: Predicate = _item1
+	val item2: Predicate = _item2
+	def test(values: Map[Symbol[_],_]): Boolean = item1.asInstanceOf[Predicate].test(values) && item2.asInstanceOf[Predicate].test(values)
+}
+
+case class Or(_item1: Predicate, _item2: Predicate) extends Predicate2[Predicate,Predicate] {
+	val item1: Predicate = _item1
+	val item2: Predicate = _item2
+	def test(values: Map[Symbol[_],_]): Boolean = item1.asInstanceOf[Predicate].test(values) || item2.asInstanceOf[Predicate].test(values)
+}
+
+// Service predicates
+case class PredicateService1[T](_item: Symbol[T]) extends Predicate1[Symbol[T]] {
+	val item: Symbol[T] = _item
+	def test(values: Map[Symbol[_],_]): Boolean = true
+}
+case class PredicateService2[S,T](_item1: Symbol[S], _item2: Symbol[T]) extends Predicate2[Symbol[S],Symbol[T]] {
+	val item1: Symbol[S] = _item1
+	val item2: Symbol[T] = _item2
+	def test(values: Map[Symbol[_],_]): Boolean = true
+}
+
+trait PredicateServiceEntry
+
+class PredicateServiceEntry1[T](_implementation: ExternalService[T,Boolean], _item: Symbol[T])
+extends PredicateService1[T](_item) with ExternalServiceReference[T,Boolean] {
+	val implementation: ExternalService[T,Boolean] = _implementation
+	override def test(values: Map[Symbol[_],_]): Boolean = implementation.call(values(item).asInstanceOf[T])
+}
+class PredicateEntry1[T](implementation: ExternalService[T,Boolean]) {
+	def apply(item: Symbol[T]): PredicateServiceEntry1[T] = new PredicateServiceEntry1[T](implementation, item)
+}
+
+class PredicateServiceEntry2[S,T](_implementation: ExternalService[Tuple2[S,T],Boolean], _item1: Symbol[S], _item2: Symbol[T])
+extends PredicateService2[S,T](_item1, _item2) with ExternalServiceReference[Tuple2[S,T],Boolean] {
+	val implementation: ExternalService[Tuple2[S,T],Boolean] = _implementation
+	override def test(values: Map[Symbol[_],_]): Boolean = implementation.call((values(item1).asInstanceOf[S],values(item2).asInstanceOf[T]))	
+}
+class PredicateEntry2[S,T](implementation: ExternalService[Tuple2[S,T],Boolean]) {
+	def apply(item1: Symbol[S], item2: Symbol[T]): PredicateServiceEntry2[S,T] = new PredicateServiceEntry2[S,T](implementation, item1, item2)
 }
