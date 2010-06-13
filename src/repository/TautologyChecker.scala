6ec85c7e4d98afc954dd9a61666438db0e49b306
Matthias Tilsner <matthias@tilsner.eu>
2010-06-13 18:01:54 +0200

    Implementation of Domain Independence
    Rather than hardcoding the interference rules the Tautology Checker
    should of course use those provided in the repository

--- src/repository/TautologyChecker.scala
+++ src/repository/TautologyChecker.scala
@@ -19,29 +19,33 @@ object TautologyChecker {
 		case _											=> false
 	}
 	
+	def applicableRules(formula: AbstractPredicatesymbol): Seq[Tuple2[AbstractPredicatesymbol, AbstractPredicatesymbol]] =
+		Repository.interferenceRules.toSeq.filter(
+			(p: Tuple2[AbstractPredicatesymbol,AbstractPredicatesymbol])
+			=> contains(formula,p._1) && !contains(formula,p._2)
+		)
+	
 	def r(formula: AbstractPredicatesymbol): AbstractPredicatesymbol = formula match {
 		// domain specifc replacement rules
-		case ^(p1,p2)    if contains(formula,Predicatesymbol("IsAirportInCity13")) && contains(formula,Predicatesymbol("IsFlightFromAirport34")) && !contains(formula,Predicatesymbol("IsFlightFromCity14"))
-														=> Console.println("DEBUG A1: "+formula); ^(formula,Predicatesymbol("IsFlightFromCity14"))
-		case ^(p1,p2)	 if contains(formula,Predicatesymbol("IsFlightFromCity14")) && contains(formula,Predicatesymbol("IsEquals42")) && !contains(formula,Predicatesymbol("IsFlightFromCity12"))
-														=> Console.println("DEBUG A2: "+formula); ^(formula,Predicatesymbol("IsFlightFromCity12"))
-		
+		case ^(p1,p2) if applicableRules(formula).size > 0
+														=> ^(formula, applicableRules(formula).head._2 )
 		// general replacement rules
-		case ¬(True)									=> False
-		case ¬(False)									=> True
-		case ^(p1,¬(p2)) if (p1 == p2)					=> False
-		case ^(¬(p1),p2) if (p1 == p2)					=> False
-		case ^(¬(p1),p2) if contains(p1,p2)				=> False	
-		case ^(p1,¬(p2)) if contains(p2,p1)				=> False	
-		case ^(p,False)									=> False
-		case ^(False,p)									=> False 
-		case ^(True,True)								=> True
-		case ¬(¬(p))									=> Console.println("DEBUG B: "+formula); p
-		case ¬(p)										=> Console.println("DEBUG C: "+formula); val pn = r(p); if(p != pn) r(¬(pn)) else ¬(pn)
-		case ^(p1,p2)									=> Console.println("DEBUG D: "+formula); val p1n = r(p1); val p2n = r(p2); if(   (p1n != p1 && p1n != ^(p1,p2) && p1n != ^(p2,p1)) 
-																																	  || (p2n != p2  && p2n != ^(p1,p2) && p2n != ^(p2,p1))
-																															 	   ) r(^(p1n,p2n)) else ^(p1,p2)
-		// fallback (not successfull)
+		case ¬(True)									=>	False
+		case ¬(False)									=>	True
+		case ^(p1,¬(p2)) if (p1 == p2)					=>	False
+		case ^(¬(p1),p2) if (p1 == p2)					=>	False
+		case ^(¬(p1),p2) if contains(p1,p2)				=>	False	
+		case ^(p1,¬(p2)) if contains(p2,p1)				=>	False	
+		case ^(p,False)									=>	False
+		case ^(False,p)									=>	False 
+		case ^(True,True)								=>	True
+		case ¬(¬(p))									=>	p
+		case ¬(p)										=>	val pn = r(p); if(p != pn) r(¬(pn)) else ¬(pn)
+		case ^(p1,p2)									=>	val p1n = r(p1); val p2n = r(p2);
+															if(	(p1n != p1 && p1n != ^(p1,p2) && p1n != ^(p2,p1)) 
+																|| (p2n != p2  && p2n != ^(p1,p2) && p2n != ^(p2,p1))
+															) r(^(p1n,p2n)) else ^(p1,p2)
+		// default replacement rule
 		case _											=> formula
 	}
 	
